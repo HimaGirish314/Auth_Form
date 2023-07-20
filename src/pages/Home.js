cb1fcf2eb70596684936f6464a8ce78a6f87907a
Himagirish <Himagirish.bandi@gmail.com>
2023-07-20 17:02:38 +0530

    navbar created

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+const Home = () => {
+  return (
+    <>
+      <div>
+        <Card>
+            <Card.Body>
+                <Card.Text>
+                    <b>Welcome! A demo on ReactJS Authentication with JWT Token</b>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+      </div>
+    </>
+  )
+}
+
+export default Home
